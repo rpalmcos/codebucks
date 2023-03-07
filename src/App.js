5aa0ea90ea837899c6acd26410b5530ec9a355ee
Reece Palmer <97815200+rpalmcos@users.noreply.github.com>
2023-03-07 15:02:45 +0000

    CodeBucks

--- src/App.js
+++ src/App.js
@@ -6,15 +6,11 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import styled from "styled-components";
 
-
 function App() {
   return (
     <>
-      <>
-        <Navbar />
-      </>
+      <HomePage />
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/privacypolicy" element={<PrivacyPolicyPage />} />
         <Route path="/termsofuse" element={<TermsofUsePage />} />
         <Route path="/about" element={<AboutPage />} />
